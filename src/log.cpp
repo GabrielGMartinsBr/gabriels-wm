a4e04ea16a7cbc21357476e2889ef2de0550596e
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2023-05-15 16:52:09 -0300

    setup repo

---
+++ src/log.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void Log(const char *str) {
+  std::cout << str << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "./log.h"
+
+int main(int, char **) {
+  Log("Hello log fn 1!");
+  Log("Hello log fn 2!");
+  Log("Hello log fn 3!");
+  Log("Hello log fn 5!");
+  Log("Hello log fn 6!");
+}
