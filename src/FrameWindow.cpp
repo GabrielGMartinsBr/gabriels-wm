8abdb809fe045507d393b67b22f702c87371b729
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2023-06-21 08:13:21 -0300

    change frame window design

--- src/FrameWindow.cpp
+++ src/FrameWindow.cpp
@@ -5,26 +5,33 @@
 
 FrameWindow::FrameWindow(Display* d, Window w)
 {
-  const unsigned int BORDER_WIDTH = 3;
+  const unsigned int BORDER_WIDTH = 0;
   const unsigned long BORDER_COLOR = 0xff0000;
-  const unsigned long BG_COLOR = 0x0000ff;
+  const unsigned long BG_COLOR = 0x3f7faf;
 
   Window rootWindow = DefaultRootWindow(d);
 
   XWindowAttributes winAttrs;
   XGetWindowAttributes(d, w, &winAttrs);
 
+  const int headerHeight = 24;
+  const int borderThick = 3;
+  int topMargin = headerHeight + borderThick;
+
+  int width = winAttrs.width + borderThick * 2;
+  int height = winAttrs.height + topMargin;
+
   const Window frame = XCreateSimpleWindow(
     d, rootWindow,
     300, 100,
-    winAttrs.width, winAttrs.height,
+    width, height,
     BORDER_WIDTH, BORDER_COLOR, BG_COLOR
   );
 
   long evtMasks = SubstructureRedirectMask | SubstructureNotifyMask;
   XSelectInput(d, frame, evtMasks);
 
-  XReparentWindow(d, w, frame, 0, 0);
+  XReparentWindow(d, w, frame, borderThick, headerHeight);
 
   XMapWindow(d, frame);
 }
